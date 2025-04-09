f7488ea2742674dcc23dbeb3ff92228867a95f86
Henrik Seng <heseng@student.ethz.ch>
2025-04-09 13:36:16 -0400

    fixed tool calling

--- student/agent.py
+++ student/agent.py
@@ -48,14 +48,10 @@ class MemoryAgent:
         self.chat += prompt
         res = self.chat.complete(parse=parse, cache=True, expensive=expensive, options=options)
         if res is None:
-            #self.chat._pop_message()
-            self.chat.messages[-1]["content"]["text"] = ""
-        #self.chat._pop_message()
-        #self.chat.messages.append(self.chat.additional_res['full_message'])
-
+            # self.chat.messages[-1]["content"]["text"] = ""
+            self.chat.messages.pop()
+        
         self.use_tools()
-        #tool_messages = self.use_tools()
-        #self.add_message(tool_messages)
         return res
 
 
@@ -64,36 +60,41 @@ class MemoryAgent:
             for m in message:
                 self.add_message(m)
             return
+        if message is None:
+            return
         assert "content" in message and "role" in message, "Message must contain 'content' and 'role'"
         self.chat.messages.append(message)
 
 
-    def use_tools(self):
-        calls = self.chat.additional_res['full_message'].tool_calls
+    def use_tools(self) -> None:
+        '''
+        Returns a list of messages or an empty list.
+        '''
+        msg = self.chat.additional_res['full_message']
+        calls = msg.tool_calls
+
         if calls is None:
             return None
         
-        messages = []
         for call in calls:
             func = call.function
             name = func.name
             args = json.loads(func.arguments)
-            message = self._use_tool(func, name, args)
-            #self.add_message(message)
+            message = self._use_tool(func, name, args, call['id'])
+            self.add_message(message)
+            
+        return None
 
-            if message is not None:
-                messages.append(message)
-        
-        return messages
     
-    def _use_tool(self, func, name, args, id=None):
+    def _use_tool(self, func, name, args, tool_call_id=""):
         tool = self.tools.get(name)
         if tool is None:
             return None
         out = tool.run(**args)
+    
         message = {
-            "role": "tool",
-            "tool_call_id": id,
+            "role": "assistant",
+            "tool_call_id": tool_call_id,
             'content': {
                 'type': 'text',
                 'text': str(out)
